2804cf9826caea38b9dffb0f711e4dcbe9e91d58
DCMC95 <up2110995@myport.ac.uk>
2024-05-07 15:11:56 +0100

    Test

--- lib/RegisterScreen.dart
+++ lib/RegisterScreen.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'LoginScreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// TEST
+
 class RegisterScreen extends StatelessWidget {
   final TextEditingController accountIdController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
